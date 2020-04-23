c8b2eea7e9bd8525fa26dfa4e63d7b6a09ac0b93
John Sandilands <johnsandilands@Johns-MacBook-Air-3.local>
2020-04-23 16:35:30 -0700

    Add functions to Project 2

--- client/javascripts/functions.js
+++ client/javascripts/functions.js
@@ -1,5 +1,5 @@
 // jshint esversion: 6
-
+//containsDuplicates
 let containsDuplicates = s => {
   let cArr = [0, 0, 0, 0],
     code;
@@ -14,3 +14,34 @@ let containsDuplicates = s => {
   }
   return false;
 };
+
+//reverseWords1
+let revWords1 = sent => {
+  let arr = sent.split("");
+  let result = "";
+  for (var i = 0; i < arr.length; i++) {
+    result = arr[i] + result;
+  }
+  return result;
+  console.log(revWords1("Hello There"));
+};
+
+//revWords2
+let revWords2 = str => {
+  let arr = str.split("");
+  let result = "";
+  arr.forEach(char => {
+    result = char + result;
+  });
+  return result;
+  console.log(revWords2("Thursday is not fun"));
+};
+//reverseWords3
+revWords3 = str => {
+  let reversed = "";
+  for (let char of str) {
+    reversed = char + reversed;
+  }
+  return reversed;
+};
+reverse("Hello World");
